0e40e9989580fa47cba367082371ae44d7195817
Liam Kennedy <liam@lkenn.xyz>
2020-04-19 14:57:10 +0100

    testopia

--- lambda/deploy-succeeded.js
+++ lambda/deploy-succeeded.js
@@ -2,10 +2,10 @@ const https = require('https');
 
 exports.handler = async (event, context, callback) => {
   const { body } = event;
-  console.log(body);
+  const json = await body.json();
   const response = await new Promise((resolve, reject) => {
     const chatID = process.env.TG_CHAT_ID
-    const msg = body.payload.title;
+    const msg = json.payload.title;
     const uri = process.env.TG_URI.replace('CHID', chatID).replace('MSG', msg);
     console.log(uri);
     const req = https.get(uri, res => {
