ed18c4f192736d3e65a0efa6a709fb8beffa11c9
Liam Kennedy <liam@lkenn.xyz>
2020-04-19 15:11:42 +0100

    final rev of function for now?

--- lambda/deploy-succeeded.js
+++ lambda/deploy-succeeded.js
@@ -4,8 +4,8 @@ exports.handler = async (event, context, callback) => {
   const { body } = event;
   const json = JSON.parse(body);
   const response = await new Promise((resolve, reject) => {
-    const chatID = process.env.TG_CHAT_ID
     const msg = json.payload.title;
+    const chatID = msg.includes('Create Blog') ? process.env.TG_GROUP_CHAT_ID : process.env.TG_SOLO_CHAT_ID;
     const uri = process.env.TG_URI.replace('CHID', chatID).replace('MSG', msg);
     const req = https.get(uri, res => {
       res.on('end', () => {
