9acf69fb4d5643729b8c3da4cf1ffcc2c17a753d
Liam Kennedy <liam@lkenn.xyz>
2020-04-19 14:47:22 +0100

    Testing updated function

--- lambda/deploy-succeeded.js
+++ lambda/deploy-succeeded.js
@@ -4,8 +4,8 @@ exports.handler = async (event, context, callback) => {
   const { body } = event;
   const response = await new Promise((resolve, reject) => {
     const chatID = process.env.TG_CHAT_ID
-    const msg = JSON.stringify(body);
-    const uri = process.env.TG_URI.replace('MSG', msg).replace('CHID', chatID);
+    const msg = JSON.stringify(body.title);
+    const uri = process.env.TG_URI.replace('CHID', chatID).replace('MSG', msg);
     console.log(uri);
     const req = https.get(uri, res => {
       res.on('end', () => {
